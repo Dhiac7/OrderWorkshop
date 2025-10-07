04ee5ad5cba4dc4a3e4fbd55ddc634fea33d0c07
Mohamed Dhia Chaouachi <mohameddhia.chaouachi@esprit.tn>
2025-10-07 13:47:11 +0100

    testsmtp20

--- src/main/java/tn/esprit/Main.java
+++ src/main/java/tn/esprit/Main.java
@@ -20,7 +20,7 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
-        //lastTest 222516995 dddd
+        //lastTest
         launch(args); // ✅ Appel à la méthode launch() de Application
     }
 }
