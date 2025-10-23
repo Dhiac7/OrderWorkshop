815a22d8adec9870cd4738744bfba992b1d95ffc
Dhiac7 <mohameddhia.chaouachi@esprit.tn>
2025-10-23 01:18:21 +0100

    dgsd

--- src/main/java/tn/esprit/Main.java
+++ src/main/java/tn/esprit/Main.java
@@ -20,7 +20,7 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
-        //lastTest ssss
+        //lastTest ssssss
         launch(args); // ✅ Appel à la méthode launch() de Application
     }
 }
